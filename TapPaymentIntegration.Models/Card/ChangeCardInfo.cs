882bf6ee2a30f0346388ec3b38e37195a6250adb
mraasmasood1 <161988619+mraasmasood1@users.noreply.github.com>
2024-03-27 23:07:38 +0500

    push

--- TapPaymentIntegration.Models/Card/ChangeCardInfo.cs
+++ TapPaymentIntegration.Models/Card/ChangeCardInfo.cs
@@ -7,7 +7,7 @@ namespace TapPaymentIntegration.Models.Card
     {
         [Key]
         public int ChangeCardId { get; set; }
-        public string OldCardName { get; set; }
+        public string OldCardName { get; set; } 
         public string NewCardName { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
